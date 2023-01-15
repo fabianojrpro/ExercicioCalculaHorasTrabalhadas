4b6cf755d3ee44b8d7ccbda7cc4660e195756348
Fabiano Junior <99301175+fabianojrpro@users.noreply.github.com>
2023-01-15 19:40:53 -0300

    Add files via upload

---
+++ ExercicioCalculaHoraFunc/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Globalization;
+
+int numeroDoFuncionario, horasTrabalhadas;
+double valorDaHora, salario;
+
+numeroDoFuncionario = int.Parse(Console.ReadLine());
+horasTrabalhadas = int.Parse(Console.ReadLine());
+
+valorDaHora = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+salario = horasTrabalhadas * valorDaHora;
+
+Console.WriteLine("NUMBER = "+numeroDoFuncionario);
+Console.WriteLine("SALARY = R$ " + salario.ToString("F2", CultureInfo.InvariantCulture));
